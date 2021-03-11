24a09540e402488e6db47244e0b1a0dff83e557e
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-03-11 12:04:34 +0300

    Add django-debug-toolbar

--- yatube/settings.py
+++ yatube/settings.py
@@ -20,6 +20,11 @@ ALLOWED_HOSTS = [
 ]
 
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -33,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
+    'debug_toolbar',
+    # 'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -43,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
--- yatube/urls.py
+++ yatube/urls.py
@@ -23,3 +23,5 @@ if settings.DEBUG:
                           document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
+    import debug_toolbar
+    urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),) 
