b5283f17e6bc9b7a63566e447bbe49bd8e40b319
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-04-02 00:48:16 +0300

    Add URL path for profile_edit

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("signup/", views.SignUp.as_view(), name="signup")
+    path("signup/",
+         views.SignUp.as_view(),
+        name="signup"),
+    path("profile_edit/",
+         views.UserEditView.as_view(),
+         name="profile_edit"),
 ]
