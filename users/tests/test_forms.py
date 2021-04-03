b7ece79533d61eb83af94e9d8eec4b413e10efa0
Ilya Boyur <ilya.boyur.r@yandex.ru>
2021-04-03 15:41:39 +0300

    Fix test errors

--- users/tests/test_forms.py
+++ users/tests/test_forms.py
@@ -12,9 +12,12 @@ from users.models import Profile, User
 
 
 USER_NAME = "Test User"
+USER_NAME_WITH_PROFILE = "Test User With Profile"
+USER_NAME_ADDS_PROFILE = "Test User who adds profile in test"
 PROFILE_CREATE_URL = reverse("profile_create")
 PROFILE_EDIT_URL = reverse("profile_edit")
 PROFILE_URL = reverse("profile", args=[USER_NAME])
+PROFILE_WITH_PROFILE_URL = reverse("profile", args=[USER_NAME_WITH_PROFILE])
 
 SMALL_GIF_CONTENT = (b'\x47\x49\x46\x38\x39\x61\x02\x00'
                      b'\x01\x00\x80\x00\x00\x00\x00\x00'
@@ -25,22 +28,50 @@ SMALL_GIF_CONTENT = (b'\x47\x49\x46\x38\x39\x61\x02\x00'
 
 
 @override_settings(MEDIA_ROOT=tempfile.mkdtemp(dir=settings.BASE_DIR))
-class UserProfileEditFormTests(TestCase):
+class ProfileFormTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.user = User.objects.create_user(username=USER_NAME)
-        Profile.objects.create(user=cls.user)
+        cls.user_with_profile = User.objects.create_user(
+            username=USER_NAME_WITH_PROFILE
+        )
+        Profile.objects.create(user=cls.user_with_profile)
         cls.guest_client = Client()
         cls.authorized_client = Client()
         cls.authorized_client.force_login(user=cls.user)
+        cls.authorized_client_with_profile = Client()
+        cls.authorized_client_with_profile.force_login(
+            user=cls.user_with_profile
+        )
 
     @classmethod
     def tearDownClass(cls):
         shutil.rmtree(settings.MEDIA_ROOT, ignore_errors=True)
         super().tearDownClass()
 
-    def test_update_user(self):
+    def test_profile_create(self):
+        """Валидная форма создает 'profile'."""
+        user = User.objects.create(username=USER_NAME_ADDS_PROFILE)
+        client = Client()
+        client.force_login(user=user)
+        form_data = {
+            "bio": "Биография",
+            "image": SimpleUploadedFile("small_forms.gif",
+                                        content=SMALL_GIF_CONTENT,
+                                        content_type="image/gif"),
+        }
+        response = client.post(
+            PROFILE_CREATE_URL,
+            data=form_data,
+            follow=True,
+        )
+        self.assertRedirects(response, reverse("profile", args=[USER_NAME_ADDS_PROFILE]))
+        self.assertTrue(hasattr(response.context["author"], 'profile'))
+        self.assertEqual(response.context["author"].profile.bio, form_data["bio"])
+        self.assertEqual(response.context["author"].profile.image, "profiles/small_forms.gif")
+
+    def test_profile_edit(self):
         """Валидная форма обновляет 'profile'."""
         form_data = {
             "bio": "Биография после редактирования",
@@ -50,12 +81,12 @@ class UserProfileEditFormTests(TestCase):
                 content_type="image/gif"
             ),
         }
-        response = self.authorized_client.post(
-            PROFILE_CREATE_URL,
+        response = self.authorized_client_with_profile.post(
+            PROFILE_EDIT_URL,
             data=form_data,
             follow=True,
         )
-        self.assertRedirects(response, PROFILE_URL)
+        self.assertRedirects(response, PROFILE_WITH_PROFILE_URL)
         # Получаем обновленный профиль
         updated_profile = response.context["author"].profile
         self.assertEqual(updated_profile.bio, form_data["bio"])
@@ -64,9 +95,23 @@ class UserProfileEditFormTests(TestCase):
             "profiles/small_forms_update.gif"
         )
 
-    def test_update_user_guest(self):
+    def test_profile_create_guest(self):
+        """Гость не может создать 'profile'."""
+        profile_count = Profile.objects.count()
+        self.guest_client.post(
+            PROFILE_CREATE_URL,
+            data={
+                "bio": "Новая биография",
+                "image": SimpleUploadedFile("small_forms_update.gif",
+                                            content=SMALL_GIF_CONTENT,
+                                            content_type="image/gif"),
+            },
+        )
+        self.assertEqual(profile_count, Profile.objects.count())
+
+    def test_profile_edit_guest(self):
         """Гость не может редактировать 'profile'."""
-        profile_before_edit = Profile.objects.get(user=self.user)
+        profile_before_edit = Profile.objects.get(user=self.user_with_profile)
         self.guest_client.post(
             PROFILE_EDIT_URL,
             data={
@@ -76,7 +121,7 @@ class UserProfileEditFormTests(TestCase):
                                             content_type="image/gif"),
             },
         )
-        profile_after_edit = Profile.objects.get(user=self.user)
+        profile_after_edit = Profile.objects.get(user=self.user_with_profile)
         self.assertEqual(
             profile_before_edit,
             profile_after_edit
@@ -86,12 +131,12 @@ class UserProfileEditFormTests(TestCase):
         """Типы полей формы в словаре 'context' в ответе по URL-адресу
         соответствуют ожиданиям."""
         urls = [
-            PROFILE_CREATE_URL,
-            PROFILE_EDIT_URL,
+            [PROFILE_CREATE_URL, self.authorized_client],
+            [PROFILE_EDIT_URL, self.authorized_client_with_profile],
         ]
-        for url in urls:
+        for url, client in urls:
             with self.subTest(value=url):
-                response = self.authorized_client.get(url)
+                response = client.get(url)
                 form_fields = {
                     "bio": forms.fields.CharField,
                     "image": forms.fields.ImageField,
@@ -100,3 +145,32 @@ class UserProfileEditFormTests(TestCase):
                     with self.subTest(value=value):
                         form_field = response.context["form"].fields.get(value)
                         self.assertIsInstance(form_field, expected_type)
+
+
+
+
+
+# class  ProfileEditView(UpdateView):
+#     form_class = ProfileForm
+#     success_url = reverse_lazy("index")
+#     template_name = "registration/profile_edit.html"
+
+#     def get_object(self):
+#         return self.request.user.profile
+
+
+# class  ProfileCreateView(CreateView):
+#     form_class = ProfileForm
+#     success_url = reverse_lazy("index")
+#     template_name = "registration/profile_create.html"
+
+#     def form_valid(self, form):
+#         form.instance.user = self.request.user
+#         return super().form_valid(form)
+
+            # path("profile_create/",
+    #      views.ProfileCreateView.as_view(),
+    #      name="profile_create"),
+    # path("profile_edit/",
+    #      views.ProfileEditView.as_view(),
+    #      name="profile_edit"),
--- users/tests/test_urls.py
+++ users/tests/test_urls.py
@@ -5,8 +5,7 @@ from django.urls import reverse
 
 
 USER_NAME = "Test User"
-USER_NAME_OTHER = "Test User Other"
-
+USER_NAME_WIH_PROFILE = "Test User With Profile"
 # STATIC URLS
 PROFILE_CREATE_URL = reverse("profile_create")
 PROFILE_EDIT_URL = reverse("profile_edit")
@@ -18,15 +17,17 @@ class UsersURLTests(TestCase):
     def setUpClass(cls):
         super().setUpClass()
         user = User.objects.create_user(username=USER_NAME)
-        user_other = User.objects.create_user(username=USER_NAME_OTHER)
+        user_with_profile = User.objects.create_user(
+            username=USER_NAME_WIH_PROFILE
+        )
         Profile.objects.create(
-            user=user_other,
+            user=user_with_profile,
         )
         cls.guest_client = Client()
         cls.authorized_client = Client()
         cls.authorized_client.force_login(user)
         cls.authorized_client_with_profile = Client()
-        cls.authorized_client_with_profile.force_login(user_other)
+        cls.authorized_client_with_profile.force_login(user_with_profile)
     
     def test_posts_url_exists_at_desired_location(self):
         """Страницы возвращают ожидаемый код ответа
@@ -47,8 +48,16 @@ class UsersURLTests(TestCase):
         """URL-адрес использует соответствующий ему html-шаблон,
         если у пользователя есть права на просмотр страниц по данным URL."""
         url_templates = [
-            [PROFILE_CREATE_URL, self.guest_client, "profile_new.html"],
-            [PROFILE_EDIT_URL, self.guest_client, "profile_new.html"],
+            [
+                PROFILE_CREATE_URL,
+                self.authorized_client,
+                "registration/profile_new.html"
+            ],
+            [
+                PROFILE_EDIT_URL,
+                self.authorized_client_with_profile,
+                "registration/profile_new.html"
+            ],
         ]
         for url, client, template in url_templates:
             with self.subTest(value=url):
